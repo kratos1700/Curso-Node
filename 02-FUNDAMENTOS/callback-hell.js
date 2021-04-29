77671c5840a817722b16be56342095e0f8b0eee8
Kratos1700 <hunterstarsa@hotmail.com>
2021-04-29 20:55:54 +0200

    no message

--- 02-FUNDAMENTOS/callback-hell.js
+++ 02-FUNDAMENTOS/callback-hell.js
@@ -1,4 +1,4 @@
-// callback anidados
+// callback anidados no usarse 
 
 // arreglo empleados
 const empleados = [
@@ -28,11 +28,13 @@ const salarios = [
 
 ]
 
+const id= 3;
+
 // funcion con callback para retornan un empleado por id
 const getEmpleado = (id, callback) => {
 
     // .find  arreglos, pasamos el id del parametro para buscar el id del arreglo
-    const empleado = empleados.find((e) => e.id === id)
+    const empleado = empleados.find((e) => e.id === id)?.nombre
 
     if (empleado) {
         //  el null es como un false al ejecutar el callback
@@ -43,15 +45,37 @@ const getEmpleado = (id, callback) => {
 
 
 }
+const getSalario = (id, callback) =>{
+    const salario = salarios.find( s => s.id === id)?.salario
+    if(salario){
+        callback (null, salario);
+    }else{
+        callback(`El salario del id ${id} no existe`)
+    }
+}
 
 // el callback se le pasa un error y un objeto 
-getEmpleado(7, (err,empleado)=> {
+getEmpleado(id, (err,empleado)=> {
     // comprobamos que el usuario esiste, si hay error es que no existe
     if (err){
         console.log('ERROR!!');
         return console.log(err);
-    }else{
+    }/*else{
         // mostramos mensaje que si existe el usuario
-        console.log('el usuario existe')
-    }
+        console.log('el usuario existe ')
+        console.log(empleado.nombre)
+    }*/
+    getSalario(id,(err, salario)=> {
+        if(err){
+            
+            console.log(err);
+        }
+            
+            console.log(`El empleado:`, empleado, `tiene un salario de: `, salario)
+        
+    })
 })
+
+
+
+
---
+++ 02-FUNDAMENTOS/promesas.js
@@ -0,0 +1,27 @@
+// arreglo empleados
+const empleados = [
+    {
+        id: 1,
+        nombre: 'Kratos'
+    },
+    {
+        id: 2,
+        nombre: 'Juanito'
+    },
+    {
+        id: 3,
+        nombre: 'chiquilicuatre'
+    },
+]
+// arreglo salarios
+const salarios = [
+    {
+        id: 1,
+        salario: 1500
+    },
+    {
+        id: 2,
+        salario: 1000
+    },
+
+]
