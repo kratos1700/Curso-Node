bfd29b6f4662a460eb0bb09136d95c742cb803d0
Kratos1700 <hunterstarsa@hotmail.com>
2021-05-02 18:32:07 +0200

    importando dependencias : yargs

--- 03-BASES-NODE/app.js
+++ 03-BASES-NODE/app.js
@@ -1,16 +1,31 @@
 // https://nodejs.org/es/
+// https://www.npmjs.com
 
 
 // importamos las ayudas multiplicar
-const {crearArchivo}= require('./ayudas/multiplicar')
+const {crearArchivo}= require('./ayudas/multiplicar');
+const argv = require('yargs').argv;
 
 console.clear();
 
+// descomponemos el argumento
+//const [ , ,arg3= 'base=5'] = process.argv
+// descomponemos el argumento 3 desde el = i nos quedamos la parte de la derecha
+//const[ ,num] = arg3.split('=')
 
-let num= 6;
+console.log(process.argv);
+console.log(argv);
 
-crearArchivo(num)
+console.log('num: yargs', argv.num);
+
+
+//console.log(num);
+
+
+//let num= 6;
+
+/*crearArchivo(num)
 .then(nombreArchivo => console.log(nombreArchivo, 'creada'))
 .catch(err => console.log (err));
-
+*/
 
