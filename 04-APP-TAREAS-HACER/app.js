aa628fdfabad2d7576a9cc20690dd8547164acec
Kratos1700 <hunterstarsa@hotmail.com>
2021-05-05 16:07:27 +0200

    implementacion de la eliminacion de tareas

--- 04-APP-TAREAS-HACER/app.js
+++ 04-APP-TAREAS-HACER/app.js
@@ -1,9 +1,11 @@
 
-require ('colors');
+require('colors');
 const { guardarDB, leerDB } = require('./ayudas/guardarArchivo');
 const { inquirerMenu,
-        pausa,
-        leerInput } = require('./ayudas/inquirer');
+  pausa,
+  leerInput,
+  listadoTareasBorrar,
+  confirmar } = require('./ayudas/inquirer');
 const Tareas = require('./models/tareas');
 const Tarea = require('./models/tareas');
 // la importacion de la funcion se hace auto  al llamar la funcion
@@ -12,55 +14,81 @@ const Tarea = require('./models/tareas');
 
 console.clear();
 
-const main = async() => {
-    
+const main = async () => {
 
-    let opt='';
-    // instanciamos las tareas se guardara por uid, tipo firebase i mongodb
-    const tareas = new Tareas();
 
-    const tareasDB = leerDB();
+  let opt = '';
+  // instanciamos las tareas se guardara por uid, tipo firebase i mongodb
+  const tareas = new Tareas();
 
-    // cargamos los datos del archivo JSON
-    if (tareasDB){
-        tareas.cargarTareasFromArray(tareasDB);
-    }
+  const tareasDB = leerDB();
 
-    
-    do{
+  // cargamos los datos del archivo JSON
+  if (tareasDB) {
+    tareas.cargarTareasFromArray(tareasDB);
+  }
 
-       
-        // con el await esperamos a que devolvamos 
-        //creamos el menu y guardamos la opcion seleccionada
-      opt =  await  inquirerMenu();
-        switch(opt){
-            case '1':
-                // guardamos texto que introduce el usuario
-                const desc = await leerInput('descripción: ');
-                // guardamos la desc en el listado de tareas
-                tareas.crearTarea(desc);
-                break;
-            case'2':
-            // mostramos el listado de tareas
-            //console.log(tareas._listado);
-           tareas.listadocompleto()
-           // console.log(tareas.liastadoArray)
-                break;
-            
+
+  do {
+
+
+    // con el await esperamos a que devolvamos 
+    //creamos el menu y guardamos la opcion seleccionada
+    opt = await inquirerMenu();
+    switch (opt) {
+      case '1':
+        // guardamos texto que introduce el usuario
+        const desc = await leerInput('descripción: ');
+        // guardamos la desc en el listado de tareas
+        tareas.crearTarea(desc);
+        break;
+      case '2':
+        // mostramos el listado de tareas
+        tareas.listadocompleto()
+        break;
+      case '3':
+        // mostramos el listado de tareas completadas
+        tareas.listarPendientesCompletadas(true)
+        break;
+      case '4':
+        // mostramos el listado de tareas no completadas
+        tareas.listarPendientesCompletadas(false)
+        break;
+      case '5':
+        // mostramos el listado de tareas
+        tareas.listarPendientesCompletadas(false)
+        break;
+      case '6':
+        // eliminar tarea, se tiene que esperar a que se cree el menu
+        const id = await listadoTareasBorrar(tareas.liastadoArray);
+
+        if (id !== '0') {
+          // guardamos la seleccion del usuario
+          const confirmacion = await confirmar('Estas seguro de eliminar la tarea??'.red);
+          // si es true borramos pasandole el id
+          if (confirmacion) {
+            tareas.borrarTarea(id);
+            console.log('Tarea eliminada correctamente'.green);
+          }
+
         }
 
-        // guardamos las tareas
-        guardarDB(tareas.liastadoArray)
+        break;
 
-      await pausa();
-      // si la opcion es igual a 0 salimos directamente
+    }
+
+    // guardamos las tareas
+    guardarDB(tareas.liastadoArray)
+
+    await pausa();
+    // si la opcion es igual a 0 salimos directamente
     // if(opt !=='0') await pausa() ;
 
-    }while(opt !=='0')
-    
-   
+  } while (opt !== '0')
 
-   // pausa();
+
+
+  // pausa();
 }
 
 main()
--- 04-APP-TAREAS-HACER/models/tareas.js
+++ 04-APP-TAREAS-HACER/models/tareas.js
@@ -1,67 +1,109 @@
 // importamos la clase tarea
 const Tarea = require('./tarea');
-require ('colors');
+require('colors');
 
 // clase tareas
-class Tareas{
+class Tareas {
 
-   _listado = {};
+    _listado = {};
 
-   // metodo get
-   get liastadoArray(){
-       const listado =[];
-       //extraemos las llaves de cada objeto
-       Object.keys(this._listado).forEach(key =>{
+    // metodo get
+    get liastadoArray() {
+        const listado = [];
+        //extraemos las llaves de cada objeto
+        Object.keys(this._listado).forEach(key => {
 
-        const tarea = this._listado[key];
-        // añadimos la tarea al listado
-        listado.push(tarea);
-           
-
-       });
-       // retornamo el arreglo
-       return listado;
-   }
-
-   constructor(){
-       this._listado={};
-   }
-
-   // funcion para retornar los elementos del array
-   cargarTareasFromArray(tareas = []){
-  
-      tareas.forEach(tarea => {
-        this._listado[tarea.id] = tarea;    
-      });
-       
-   }
-
-   // funcion para mostrar en pantalla las tareas
-   listadocompleto(){
-
-    // recorremos el arreglo de tareas
-    this.liastadoArray.forEach((tarea, i) =>{
-        // para iniciar el indice con el 1 en lugar de  0 y ponemos en color verde
-        const idx = `${i+1}`.green;
-        // desestructuramos la descripcion y completado en
-        const {desc, completadoEn} = tarea;
-        // comprobamos que completado  existe, asignamos el verde a completado y rojo a pendiente
-        const estado = ( completadoEn) ? 'Completada'.green : 'Pendiente'.red; 
-
-        console.log(`${idx}  ${desc} ::   ${estado}`);
-
-    });
-
-   }
+            const tarea = this._listado[key];
+            // añadimos la tarea al listado
+            listado.push(tarea);
 
 
-   // funcion para crear tareas pasamos por parametro la desc
-   crearTarea(desc = ''){
-       // creamos una nueva tarea con la descripcion
-       const tarea = new Tarea(desc);
+        });
+        // retornamo el arreglo
+        return listado;
+    }
+
+    constructor() {
+        this._listado = {};
+    }
+
+    // funcion para retornar los elementos del array
+    cargarTareasFromArray(tareas = []) {
+
+        tareas.forEach(tarea => {
+            this._listado[tarea.id] = tarea;
+        });
+
+    }
+
+    // funcion para mostrar en pantalla las tareas
+    listadocompleto() {
+        // recorremos el arreglo de tareas
+        this.liastadoArray.forEach((tarea, i) => {
+            // para iniciar el indice con el 1 en lugar de  0 y ponemos en color verde
+            const idx = `${i + 1}.`.green;
+            // desestructuramos la descripcion y completado en
+            const { desc, completadoEn } = tarea;
+            // comprobamos que completado  existe, asignamos el verde a completado y rojo a pendiente
+            const estado = (completadoEn) ? 'Completada'.green : 'Pendiente'.red;
+            // mostramos la lista
+            console.log(`${idx}  ${desc} ::   ${estado}`);
+        });
+
+    }
+
+    // funcion para mostrar las listas completadas y pendientes
+    listarPendientesCompletadas(completadas = true) {
+        let contador = 0;
+        // recorremos el arreglo de tareas
+        this.liastadoArray.forEach(tarea => {
+
+            // desestructuramos la descripcion y completado en
+            const { desc, completadoEn } = tarea;
+            // comprobamos que completado  existe, asignamos el verde a completado y rojo a pendiente
+            const estado = (completadoEn) ? 'Completada'.green : 'Pendiente'.red;
+
+
+            if (completadas) {
+
+                if (completadoEn) {
+                    contador += 1;
+                    // mostramos la lista
+                    console.log(`${contador.toString().green}.  ${desc} ::   ${completadoEn}`);
+                }
+
+            } else {
+                if (!completadoEn) {
+                    contador += 1;
+                    // mostramos la lista
+                    console.log(`${contador.toString().red}.  ${desc} ::   ${estado}`);
+                }
+
+            }
+
+
+        });
+
+    }
+
+    // funcion para borrar una tarea
+    borrarTarea(id = ''){
+
+        // compreobamos que exista la tarea
+        if( this._listado[id]){
+            delete this._listado[id];
+        }
+
+    }
+
+
+    // funcion para crear tareas pasamos por parametro la desc
+    crearTarea(desc = '') {
+        // creamos una nueva tarea con la descripcion
+        const tarea = new Tarea(desc);
         // la id sera la ref de la lista que nos apuntara a la tarea 
-       this._listado[tarea.id] = tarea;
-   }
+        this._listado[tarea.id] = tarea;
+    }
 
 }
 
