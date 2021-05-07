ff183a1ebb01f3dbf63f1936a957ce1b4f1c0be8
Kratos1700 <hunterstarsa@hotmail.com>
2021-05-07 21:25:15 +0200

    ejemplo con express

--- 06-WEBSERVER/app.js
+++ 06-WEBSERVER/app.js
@@ -1,9 +1,17 @@
 
 const express = require('express')
 const app = express()
+const port = 8080
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(8080)
+app.get('/',  (req, res) => {
+  res.send('Hola Bixitooo')
+});
+
+
+app.get('*',  (req, res) => {
+    res.send('404 Pagina no encontrada ')
+  });
+
+  app.listen(port, () => {
+    console.log(`Se esta escuchando por:  http://localhost:${port}`)
+  })
