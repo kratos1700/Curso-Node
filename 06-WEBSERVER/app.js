7208b23241dd1f7420854c38f82ca1f4480d8cc7
Kratos1700 <hunterstarsa@hotmail.com>
2021-05-08 11:26:53 +0200

    ejemplo sitio web estatico

--- 06-WEBSERVER/app.js
+++ 06-WEBSERVER/app.js
@@ -2,16 +2,24 @@
 const express = require('express')
 const app = express()
 const port = 8080
+
+// para servir contenido estatico
+// esto tiene preerencia a las linias siguientes  app.get('/')...
+app.use(express.static('public'))
  
-app.get('/',  (req, res) => {
+/* app.get('/',  (req, res) => {
   res.send('Hola Bixitooo')
-});
+}); */
 
-
+// muestra la paguina de error si no hay nada a mostrar
 app.get('*',  (req, res) => {
-    res.send('404 Pagina no encontrada ')
+    // con sendfile enviamos el archivo
+    // con __dirname creamos el path donde se encuentra
+    res.sendFile(__dirname + '/public/404.html')
   });
 
   app.listen(port, () => {
     console.log(`Se esta escuchando por:  http://localhost:${port}`)
   })
+
+  
