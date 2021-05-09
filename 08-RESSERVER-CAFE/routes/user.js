6edc5ba8a5e600b729e291f6b24062e6644d9927
Kratos1700 <hunterstarsa@hotmail.com>
2021-05-09 18:59:03 +0200

    validacion de los atributos, nombre, pass, mail, rol

--- 08-RESSERVER-CAFE/routes/user.js
+++ 08-RESSERVER-CAFE/routes/user.js
@@ -20,8 +20,15 @@ rutas.put('/:id',usuariosPut);
 
 // el midd se pasa en el 1er parameto si se usan mas de 1 se pasa por arrego
 // comprobamos el correo con check de express validator, se le pasa que se quiere comprobar y el mensage
+//con .not().isEmpty() comprobamos que no sea nulo
+rutas.post('/', [ 
+        check ('nombre', 'El nombre es obligatorio.').not().isEmpty(),
+        check ('password', 'El password debe tener mas de 6 caracteres.').isLength({min:6}),
+        check ('correo', 'El correo no es valido.').isEmail(),
+        check ('rol', 'No es un rol Valido').isIn(['ADMIN_ROLE','USER_ROLE'])
 
-rutas.post('/', [ check ('correo', 'El correo no es valido.').isEmail()]
+
+]
 ,usuariosPost);
 
 rutas.patch('/', usuariosPatch);
